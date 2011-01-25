e86c951ee5f241763ac26378a06d95b181c8cb0d
Ian Hall <ian@flamingballofwreckage.net>
2011-01-25 17:30:14 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freemuseums::Application.config.secret_token = 'e47ddf872a5cee630462ea1f534b503e8f7030782ba2eb4e9cba00aa09d274be0734e6839d3e9c67507671eb9d5e9bb57fa9ca7b4cc4bf10b0478d1baf1d816a'
